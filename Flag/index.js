7dc2956b0e37a4c5dc349399490f6287b0670c81
Baskar Yabase <baymax1298@gmail.com>
2021-08-15 09:56:36 +0530

    Changes to the env

--- Flag/index.js
+++ Flag/index.js
@@ -1,12 +1,21 @@
-function hoist(){
-    for (let i = 60; i > 0; i--) {
+function hoist() {
+    for (let i = 60; i >= 0; i--) {
         let timer = (60 - i) * (30)
         setTimeout(() => {
-            console.log(timer, i)
+            console.log({ i })
             document.querySelector("#flag").style.top = i + "vh";
+            if (i == 0) {
+
+                setTimeout(() => {
+                    document.getElementById('root').innerHTML = ""
+                    document.getElementById('root').setAttribute("style", "background-image: url(" + "./independance.jpeg" + ");background-repeat: no-repeat;background-size:cover;width:100vw;height:100vh;");
+
+                }, 2000)
+            }
         }, timer)
     }
 }
+
 function unHoist() {
     document.querySelector("#flag").style.top = "60vh"
 }
