bd4d9ce2fb7396abbb604c3504548ba72c761151
Baskar Yabase <baymax1298@gmail.com>
2021-08-26 20:41:08 +0530

    Changes to the react

---
+++ basics/index.js
@@ -0,0 +1,80 @@
+// default params
+// function test(param1 = 0, param2 = 0) {
+//     return param1 + param2
+// }
+
+// console.log(test())
+
+// String literals
+// let str1 = "Baskar"
+// let str2 = "Yabase"
+// let str3 = `${str1}     ${str2}`
+// console.log(str3)
+
+// Object Destructors
+// let object1 = { baskar: "he is good", senthil: "Dont know", shabir: "nice person",sijin:"not a hero he is superhero" };
+// let { baskar, senthil } = object1
+// console.log(baskar)
+// console.log(senthil)
+
+// let array1 = [1, 2, 3, 4, 5]
+// let [index1, index2] = array1
+// console.log(index1)
+// console.log(index2)
+
+// arrow function
+
+// Promise
+// console.log("I am syncronous")
+// let p = () => new Promise((resolve, reject) => {
+//     setTimeout(()=>{
+//         // resolve("I am async done successfullt")
+//         reject("I am async done failure")
+//     },2000)
+// })
+// p().then((data) => console.log("Then",data)).catch(err => console.log("Catch",err))
+// console.log("I am syncronous")
+
+// let and const
+// const name = "Baskar"
+// name = "jio";
+// console.log(name)
+// let firstName = "Jio"
+// firstName = "jio"
+
+// // class
+// class Person {
+//     constructor(firstName,lastName){
+//         this.firstName = firstName
+//         this.lastName = lastName
+//     }
+//     firstName = ""
+//     lastName = ""
+//     setName = (firstName, lastName) => {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+//     getName = ()=>{
+//         return `${this.firstName} ${this.lastName}`
+//     }
+// }
+// let p1 = new Person("Baskar","Yabase")
+// let p2 = new Person("John","Disilwa")
+// console.log(p1)
+// console.log(p2)
+
+
+// let obj = {
+//     key1:"val1",
+//     key2:"val2",
+//     key3:"val3",
+// }
+
+// let keys = Object.keys(obj)
+// let values = Object.values(obj)
+// let entries = Object.entries(obj)
+
+// console.log(keys)
+// console.log(values)
+// console.log(entries)
+
