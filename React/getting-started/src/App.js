41cc2eb3b6ea4dcb03b34b03e72df49ee380573c
Baskar Yabase <baymax1298@gmail.com>
2021-09-01 21:33:21 +0530

    Changes to the calculator init

--- React/getting-started/src/App.js
+++ React/getting-started/src/App.js
@@ -1,14 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const operators = ["+", "-", "*", "/"]
 function App() {
+  const [state, setState] = useState("");
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="display">
+          {state}
+        </div>
+        
+       <div className="row">
+       {numbers.map((num) => {
+          return <div className="col-4 d-inline-block">
+            <button onClick={(e) => setState(state + num)} type="button" className="btn btn-default">{num}</button>
+          </div>
+        })}
+        {operators.map((operator)=>{
+          return <div className="col-4 d-inline-block">
+            <button onClick={(e) => setState(`${state}${operator}`)} type="button" className="btn btn-default">{operator}</button>
+        </div>
+        })}
+        <div className="col-4 d-inline-block">
+            <button onClick={e=>{
+              let total = eval(state);
+              setState(`${total}`);
+            }} type="button" className="btn btn-default">=</button>
+        </div>
+        <div className="col-12 d-inline-block">
+            <button onClick={(e) => setState("")} type="button" className="btn btn-default col-12">CLEAR</button>
+        </div>
+       </div>
         <a
           className="App-link"
           href="https://reactjs.org"
